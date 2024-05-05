270e93c8c24dc8b59ccb4098eb66256a1fb5fa37
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-05-05 17:08:23 +0700

    up

--- CollectionsAll/src/main/java/Car.java
+++ CollectionsAll/src/main/java/Car.java
@@ -1,7 +1,11 @@
+import java.util.Objects;
+
 public class Car {
+
     private String brand;
     private int number;
-    public Car(String brand, int number){
+
+    public Car(String brand, int number) {
         this.brand = brand;
         this.number = number;
     }
@@ -12,5 +16,22 @@ public class Car {
 
     public int getNumber() {
         return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return number == car.number && Objects.equals(brand, car.brand);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, number);
     }
 }
