17f049b4bde24449ca77cefa6a530b9e26a5922a
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-05-19 18:13:13 +0700

    up

---
+++ Concurrency/src/main/java/Counter.java
@@ -0,0 +1,13 @@
+public class Counter {
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+    public void inc(){
+        value++;
+    }
+    public void dec(){
+        value--;
+    }
+}
