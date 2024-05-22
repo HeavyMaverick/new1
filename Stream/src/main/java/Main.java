9832515241fa7126a5b8c8fa667de5254a5a3129
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-05-22 22:31:48 +0700

    up

--- Stream/src/main/java/Main.java
+++ Stream/src/main/java/Main.java
@@ -1,29 +1,47 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-//        Director director = new Director();
-//        String result = director.force((int n) -> {
-//           for (int i =0; i< n; i++){
-//               System.out.println("1111");
-//           }
-//            return "Sss";
-//        }, 5);
-//        System.out.println("Result");
         List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 100; i++){
-            numbers.add((int) (Math.random() * 1000));
+        for (int i = 0; i < 1000; i++) {
+            numbers.add((int) (Math.random() * 100 + 101));
         }
-        List<Integer> filtered = filter(numbers, i -> i % 2 == 0);
-        for (int i: filtered){
-            System.out.println(i);
+        List<String> list = numbers.stream()
+                .filter(integer -> integer % 5 == 0 && integer % 2 == 0)
+                .map(Math::sqrt)
+                .map(d -> "Корень: " + d)
+                .toList();
+        for (String s : list){
+            System.out.println(s);
         }
+//        List<String> list = numbers.stream()
+//                .filter(n -> n % 2 == 0)
+//                .map(integer -> "Number: " + integer)
+//                .filter(s -> s.endsWith("0"))
+//                .map(s -> s + "!")
+//                .collect(Collectors.toList());
+
+//        for(String s : list){
+//            System.out.println(s);
+//        }
     }
-    private static List<Integer> filter(List<Integer> list ,Predicate predicate){
+
+    private static List<String> map(List<Integer> numbers) {
+        List<String> result = new ArrayList<>();
+        for (int number : numbers) {
+            result.add("Number: " + number);
+        }
+        return result;
+    }
+
+    private static List<Integer> filter(List<Integer> list, Predicate predicate) {
         List<Integer> result = new ArrayList<>();
-        for (int i:list){
-            if (predicate.test(i)){
+        for (int i : list) {
+            if (predicate.test(i)) {
                 result.add(i);
             }
         }
