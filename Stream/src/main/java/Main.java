d5034e9408fb1c7baad71df3713deafa4f3e60bd
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-05-23 20:56:56 +0700

    up

--- Stream/src/main/java/Main.java
+++ Stream/src/main/java/Main.java
@@ -1,50 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
-            numbers.add((int) (Math.random() * 100 + 101));
+        List<Float> numbers = new ArrayList<>();
+        for (int i = 0; i < 30_000_000; i++) {
+            numbers.add((float) i);
         }
-        List<String> list = numbers.stream()
-                .filter(integer -> integer % 5 == 0 && integer % 2 == 0)
-                .map(Math::sqrt)
-                .map(d -> "Корень: " + d)
+        long before = System.currentTimeMillis();
+        numbers.parallelStream()
+                .map(number -> Math.sin(0.2f + number / 5) * Math.cos(0.2f + number/5) * Math.cos(0.4f + number / 2))
                 .toList();
-        for (String s : list){
-            System.out.println(s);
-        }
-//        List<String> list = numbers.stream()
-//                .filter(n -> n % 2 == 0)
-//                .map(integer -> "Number: " + integer)
-//                .filter(s -> s.endsWith("0"))
-//                .map(s -> s + "!")
-//                .collect(Collectors.toList());
-
-//        for(String s : list){
-//            System.out.println(s);
-//        }
-    }
-
-    private static List<String> map(List<Integer> numbers) {
-        List<String> result = new ArrayList<>();
-        for (int number : numbers) {
-            result.add("Number: " + number);
-        }
-        return result;
-    }
-
-    private static List<Integer> filter(List<Integer> list, Predicate predicate) {
-        List<Integer> result = new ArrayList<>();
-        for (int i : list) {
-            if (predicate.test(i)) {
-                result.add(i);
-            }
-        }
-        return result;
+        long after = System.currentTimeMillis();
+        System.out.println(after - before);
     }
 }
