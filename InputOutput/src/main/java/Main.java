49921b306dc8fb566873a644aad154eeb3c56551
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-05-25 15:47:05 +0700

    up

--- InputOutput/src/main/java/Main.java
+++ InputOutput/src/main/java/Main.java
@@ -1,25 +1,31 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args){
-        File directory = new File("folder1");
-        File file = new File("folder1/names1.txt");
-        directory.mkdir();
+    public static void main(String[] args) {
+        File directory = new File("folder1/folder2");
+        File file = new File("folder1/folder2/names.txt");
+        directory.mkdirs();
         try {
             file.createNewFile();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        try (InputStream inputStream = new FileInputStream(file);) {
-            int a = inputStream.read();
-            while (a != -1){
-                System.out.print(((char) a));
-                a = inputStream.read();
+        try (Reader reader = new InputStreamReader(new FileInputStream(file));) {
+            char[] array = new char[128];
+            int count = reader.read(array);
+            StringBuilder result = new StringBuilder();
+            while (count > 0){
+                result.append(new String(array, 0, count));
+                count = reader.read(array);
             }
-
-        }catch (Exception e){
-            throw new RuntimeException(e);
-
+            String[] names = result.toString().split(" ");
+            Arrays.stream(names)
+                    .filter(name -> name.contains("г"))
+                    .forEach(System.out::println);
+        } catch (Exception e){
+            e.printStackTrace();
         }
+
     }
 }
